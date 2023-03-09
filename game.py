03cb5884b7b30eeef7e095c392cde0b4a2b7141c
Ritvik Goradia <ritvikgoradia@Ritviks-MacBook-Air-2.local>
2023-03-08 19:43:16 -0600

    initial commit

---
+++ game.py
@@ -0,0 +1,12 @@
+import turtle
+  
+# making turtle object
+sc = turtle.Screen()
+  
+# setup the screen size
+sc.setup(992,540)
+  
+# set the background color
+sc.bgpic("tablepic.png")
+
+turtle.done()
