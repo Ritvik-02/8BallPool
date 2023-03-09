6b9a1ffe50d444f6f2752220fb96a59768c0dd21
Ritvik Goradia <ritvikgoradia@Ritviks-MacBook-Air-2.local>
2023-03-08 19:52:40 -0600

    ball = move

--- game.py
+++ game.py
@@ -1,12 +1,33 @@
 import turtle
   
-# making turtle object
-sc = turtle.Screen()
-  
-# setup the screen size
-sc.setup(992,540)
-  
-# set the background color
-sc.bgpic("tablepic.png")
+def moving_object(move):
+    move.fillcolor('red') 
+    move.begin_fill()
+    move.circle(20)
+    move.end_fill()
 
-turtle.done()
+if __name__ == "__main__" :
+    
+    screen = turtle.Screen()
+    screen.setup(992,540)
+    screen.bgpic("tablepic.png")   
+
+    screen.tracer(0)           
+
+    move = turtle.Turtle() 
+    move.color('red')
+    move.speed(0) 
+    move.width(2)     
+    move.hideturtle()        
+    move.penup()            
+    move.goto(-250, 0) 
+    move.pendown()             
+
+    # infinite loop
+    while True :
+        move.clear()  
+        moving_object(move)   
+        screen.update()    
+        move.forward(0.5)
+    
+    turtle.done()
