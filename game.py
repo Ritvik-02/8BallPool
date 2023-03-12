25d06b0c7d32e1c2381f57ab8f2c7366648d63c1
Ritvik Goradia <ritvikgoradia@Ritviks-MacBook-Air-2.local>
2023-03-12 03:38:28 -0500

    added moving ball

--- game.py
+++ game.py
@@ -1,33 +1,57 @@
-import turtle
-  
-def moving_object(move):
-    move.fillcolor('red') 
-    move.begin_fill()
-    move.circle(20)
-    move.end_fill()
+from tkinter import *
+from PIL import ImageTk, Image
+
+def callback(event):
+    global dx
+    global dy
+    dx = 5
+    dy = 5
+
+def move_ball():
+    global dx
+    global dy
+
+    (left, top, right, bot) = canvas.coords(ball)
+
+    if top <= 55 or bot >= 485:
+        dy = -dy
+    if left <= 55 or right >= 937:
+        dx = -dx
+
+    canvas.move(ball, dx, dy)
+
+    if dx > 0:
+        dx -= 0.01
+    elif dx < 0:
+        dx += 0.01
+
+    if dy > 0:
+        dy -= 0.01
+    elif dy < 0:
+        dy += 0.01
+
+    tk.after(20, move_ball)
+
 
 if __name__ == "__main__" :
     
-    screen = turtle.Screen()
-    screen.setup(992,540)
-    screen.bgpic("tablepic.png")   
+    tk = Tk()
+    tk.title("8-Ball Pool")
+    tk.resizable(False, False)
+    HEIGHT = 540
+    WIDTH = 992
 
-    screen.tracer(0)           
+    canvas = Canvas(tk, width=WIDTH, height=HEIGHT)
+    img = ImageTk.PhotoImage(Image.open("./tablepic.png"))
+    canvas.create_image(496,0, anchor=N, image=img)
+    canvas.pack()
 
-    move = turtle.Turtle() 
-    move.color('red')
-    move.speed(0) 
-    move.width(2)     
-    move.hideturtle()        
-    move.penup()            
-    move.goto(-250, 0) 
-    move.pendown()             
+    ball = canvas.create_oval(100,100,125,125,fill="black")
+    dx = 5
+    dy = 5
 
-    # infinite loop
-    while True :
-        move.clear()  
-        moving_object(move)   
-        screen.update()    
-        move.forward(0.5)
-    
-    turtle.done()
+    canvas.bind("<Button-1>", callback)
+
+    tk.after(20, move_ball)
+
+    tk.mainloop()
